078f873fc90a2bacecb44f0f2abab9560e7f0ea6
Turgor <nikworkmail@mail.ru>
2019-07-28 03:55:54 +0300

    Empty project

---
+++ Minimax first try/Minimax first try/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimax_first_try
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
